1c93c6501817294bb4b0a8ebb502513a1b114de0
Simón Cuartas Rendón <scuartasr@unal.edu.co>
2024-11-22 20:43:12 -0500

    Solución de todas las actividades

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -5,28 +5,57 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-def lectura_datos(direccion, n, m):
+def lectura_datos(direccion: str, n: int, m: int):
 
     df = open(direccion, 'r').readlines()
     df = [x.replace('\n', '') for x in df]
     df = [x.split('\t') for x in df]
 
-    col_n = [x[n] for x in df]
-    col_m = [x[m] for x in df]
     #col_n = [x.split(',') for x in col_n]
 
     # Lista que genera una tupla con cada grupo de letras con el
     # número de línea
 
-    integrador = [(x[n].split(','), x[m]) for x in df]
-
-    # Lista de separa las letras de cada línea con su respectivo número
-    unificadorr = [(elemento, tupla[1]) for tupla in integrador for elemento in tupla[0]]
-    
-
-            
+    integrador = [(x[n].split(','), x[m]) for x in df]         
 
     return integrador
+
+def mapper(secuencia: list):
+    """
+    Esta función recibe una lista de tuplas, donde el primer elemento de cada
+    tupla es otra tupla. Se quiere que el elemento de cada tupla se empareje
+    con el segundo elemento de la tupla.
+    """
+
+    # Lista de separa las letras de cada línea con su respectivo número
+    return [(elemento, int(tupla[1])) for tupla in secuencia for elemento in tupla[0]]
+
+def shuffle_and_sort(secuencia: list):
+    """
+    Esta función recibe una secuencia que es una lista de tuplas, y toma el primer
+    elemento de cada una de las tuplas para organizar la lista en función de la
+    llave.
+    """
+
+    return sorted(secuencia, key=lambda x: x[0])
+
+def reducer(secuencia: list) -> list:
+    """
+    Esta función recibe una lista de tuplas y la reduce, sumando el valor
+    de cada tupla que tienen la misma llave
+    """
+
+    retorno = {}
+
+    for tupla in secuencia:
+        if tupla[0] in retorno:
+            retorno[tupla[0]] += tupla[1]
+        else:
+            retorno[tupla[0]] = tupla[1]
+
+    return retorno
+
+
 
 
 def pregunta_11():
@@ -43,6 +72,12 @@ def pregunta_11():
     direccion = './files/input/data.csv'
 
     df = lectura_datos(direccion, 3, 1)
+
+    df = mapper(df)
+
+    df = shuffle_and_sort(df)
+
+    df = reducer(df)
 
     return df
 
--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -5,6 +5,94 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+def lectura_datos(direccion: str, n: int, m: int) -> list:
+    """
+    Esta función recibe un string con la dirección en la que se alojan
+    unos datos, así como las columnas número n y m que se desean
+    seleccionar. Se desea retornar una lista de tuplas donde cada
+    tupla contiene el valor de la columna n y una lista con cada
+    uno de los elementos de interés de la columna m como una lista.
+    """
+
+    df = open(direccion, 'r').readlines()
+
+    # Eliminiación de los saltos de página en cada línea
+    df = [x.replace('\n', '') for x in df]
+
+    # Creación de una lista por cada renglón, separando por tab
+    df = [x.split('\t') for x in df]
+
+    # Selección de elementos de la columna n
+    col_n = [x[n] for x in df]
+
+    # Selección de elementos de la columna m
+    col_m = [x[m] for x in df]
+
+    col_m = suma_renglon(col_m)
+      
+    return list(zip(col_n, col_m))
+
+def suma_renglon(col_m: list) -> list:
+    """
+    Esta función suma los elementos numéricos de
+    cada lista anidada dentro de otra lista,
+    y retorna una lista donde cada elemento es el
+    valor de la suma para cada renglón
+    """
+    # Separación de los elementos de cada lista anidada
+    # (renglón)
+    col_m = [x.split(',') for x in col_m]
+
+    # Lista donde se va a guardar el resultado
+    col_y = []
+
+    # Recorriendo cada lista anidada dentro de la lista
+    for sublista in col_m:
+        # Se crea una lista local donde se guardan
+        # los números de cada renglón
+        renglon = []
+
+        # Se recorren los elementos de cada renglón
+        for elemento in sublista:
+            # Se agrega a la lista local los números de ese renglón
+            renglon.append(int(elemento.split(':')[1]))
+        
+        # Se agrega a la lista general la suma de los números
+        # de cada renglón
+        col_y.append(sum(renglon))
+
+    return(col_y)
+
+def shuffle_and_sort(secuencia: list) -> list:
+    """
+    Esta función recibe una lista de tuplas y las ordena
+    de acuerdo con su primer elemento, siguiendo un criterio
+    alfanumérico
+    """
+
+    return sorted(secuencia, key=lambda x: x[0])
+
+def reducer(secuencia: list) -> dict:
+    """
+    Esta secuencia recibe una lista de tuplas y suma los valores
+    de aquellas tuplas que tengan la misma llave, y retorna un
+    diccionario con este resultado
+    """
+
+    retorno = {}
+
+    # Conteo
+
+    for tupla in secuencia:
+        # Cuando la llave de la tupla ya está...
+        if tupla[0] in retorno:
+            retorno[tupla[0]] += tupla[1]
+        else:
+        # Si ya está, créese
+            retorno[tupla[0]] = tupla[1]
+    
+    return retorno
+
 
 def pregunta_12():
     """
@@ -15,3 +103,14 @@ def pregunta_12():
     {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
 
     """
+
+    direccion = './files/input/data.csv'
+
+    df = lectura_datos(direccion, 0, 4)
+    
+    df = shuffle_and_sort(df)
+
+    df = reducer(df)
+
+    return df
+
